dd92b75b3519e7b3eccc8ccfd4a0deefad82fd69
juankaruna <juankaruna@gmail.com>
2024-12-29 21:18:05 -0600

    DAG de carga incremental para active medallion

--- DAGs/DAG_incremental_load_active_medallion.py
+++ DAGs/DAG_incremental_load_active_medallion.py
@@ -14,10 +14,20 @@ from google.auth.transport.requests import AuthorizedSession
 #######################################################################################
 # PARAMETROS
 #######################################################################################
-nameDAG           = 'DAG_incremental_load_trip_taxi_data'
+nameDAG           = 'DAG_incremental_load_active_medallion'
 project           = 'incremental-load-taxi-electrico-nyc'
 owner             = 'juankaruna'
 email             = ['juankarua@gmail.com']
+
+extract_function_url = "https://us-central1-driven-atrium-445021-m2.cloudfunctions.net/extract_incremental_load_active_medallion_vehicles"
+transform_function_url = "https://us-central1-driven-atrium-445021-m2.cloudfunctions.net/etl_inicial_active_medallion_vehicles"
+raw_datasets_path = "raw_datasets/active_medallion_vehicles/"
+current_year = str(datetime.datetime.now().year)
+current_year_month = "-".join([current_year,str(datetime.datetime.now().month)])
+suffix_file_name = "_active_medallion_vehicles.csv"
+params = {
+    'filename': "".join([raw_datasets_path,current_year,'/',current_year_month,suffix_file_name])
+}
 #######################################################################################
 
 default_args = {
@@ -32,11 +42,21 @@ default_args = {
 }
 
 def invoke_extract_function():
-    url = "https://us-central1-driven-atrium-445021-m2.cloudfunctions.net/extract_incremental_load_trip_record_data" #the url is also the target audience. 
+    url = extract_function_url #the url is also the target audience. 
     request = google.auth.transport.requests.Request()  #this is a request for obtaining the the credentials
     id_token_credentials = id_token_credential_utils.get_default_id_token_credentials(url, request=request) # If your cloud function url has query parameters, remove them before passing to the audience 
 
     resp = AuthorizedSession(id_token_credentials).request("GET", url=url) # the authorized session object is used to access the Cloud Function
+
+    print(resp.status_code) # should return 200
+    print(resp.content) # the body of the HTTP response
+
+def invoke_transform_load_function():
+    url = transform_function_url #the url is also the target audience. 
+    request = google.auth.transport.requests.Request()  #this is a request for obtaining the the credentials
+    id_token_credentials = id_token_credential_utils.get_default_id_token_credentials(url, request=request) # If your cloud function url has query parameters, remove them before passing to the audience 
+
+    resp = AuthorizedSession(id_token_credentials).request("GET", url=url, params=params) # the authorized session object is used to access the Cloud Function
 
     print(resp.status_code) # should return 200
     print(resp.content) # the body of the HTTP response
@@ -55,8 +75,12 @@ with DAG(nameDAG,
     extract_cf = PythonOperator(task_id='extract_cf',
                     python_callable=invoke_extract_function
                 )
+    
+    transform_load_cf = PythonOperator(task_id='transform_load_cf',
+                    python_callable=invoke_transform_load_function
+                )
 
     t_end = DummyOperator(task_id="end")
 
     #############################################################
-    t_begin >> extract_cf >> t_end
+    t_begin >> extract_cf >> transform_load_cf >> t_end
