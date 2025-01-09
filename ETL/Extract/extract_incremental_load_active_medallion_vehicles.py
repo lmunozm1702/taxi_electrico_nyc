228a685cfb2ee62d21ebaf21a7439d0b26ad523d
juankaruna <juankaruna@gmail.com>
2025-01-09 17:54:19 -0600

    Fix formato funciones Extract

--- ETL/Extract/extract_incremental_load_active_medallion_vehicles.py
+++ ETL/Extract/extract_incremental_load_active_medallion_vehicles.py
@@ -18,24 +18,26 @@ def get_current_month():
     """
     return str(datetime.now().month)
 
-def get_prev_month():
+def get_current_day():
     """Retorna el año en curso
     """
-    return str(datetime.now().month-1)
+    return str(datetime.now().day-1)
 
 @functions_framework.http
 def hello_http(request):
     client_soda = Socrata(DATASET_URL, None)
-    year_prev_month = get_current_year() + '-' + get_prev_month()
-    where_query = "".join(["last_updated_date > '",year_prev_month,"-30T00:00:00.000'"])
-    results = client_soda.get(DATASET_ID, where=where_query, limit=250000, order="last_updated_date DESC")
+    current_day = get_current_year() + '-' + get_current_month() + '-' + get_current_day()
+    print(current_day)
+    where_query = "".join(["last_updated_date >= '",current_day,"T00:00:00.000'"])
+    results = client_soda.get(DATASET_ID, where=where_query, limit=10000, order="last_updated_date DESC")
     field_names = results[0].keys()
 
     client_gcs = storage.Client()
     bucket = client_gcs.get_bucket('ncy-taxi-bucket')
+    year = get_current_year()
     year_month = get_current_year() + '-' + get_current_month()
     csv_name = "_".join([year_month,"active_medallion_vehicles.csv"])
-    blob = bucket.blob('/'.join(['raw_datasets/active_medallion_vehicles',csv_name])) 
+    blob = bucket.blob('/'.join(['raw_datasets/active_medallion_vehicles',year,csv_name])) 
 
     with blob.open(mode="w", encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=field_names)
