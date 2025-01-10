c528493242302e13f35e2491781a1027d00639fb
Luis Muñoz <l.munoz.m@outlook.com>
2025-01-10 15:30:17 -0300

    corrige diccionario

--- DASHBOARD/src/pages/dictionary.py
+++ DASHBOARD/src/pages/dictionary.py
@@ -1,6 +1,6 @@
 import dash
 from dash import dcc
-from dash import html
+from dash import html, dash_table
 from dash import register_page, callback
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
@@ -16,7 +16,7 @@ def json_table(table_name):
     print(table_name)
 
     #leer dataset desde archivo json en la variable df
-    df = pd.read_json('../../assets/Data_dictionary/data_dictionary.json')
+    df = pd.read_json('assets/data_dictionary.json')
 
     df_t = df.T
 
@@ -30,24 +30,16 @@ def json_table(table_name):
         for column in row['campos']:
             items = []
             if (index == table_name) | (table_name == 'Todos'):
-                items.append(html.Td([index]))
+                #items.append(index)
                 for key, value in column.items():                
                     if key != 'valores':
-                        items.append(html.Td([value]))
-                rows.append(html.Tr(items))
-
-    table = dbc.Table(
-        table_header + [html.Tbody(rows)],
-        bordered=True,
-        dark=False,
-        hover=True,
-        responsive=True,
-        striped=True,
-    )  
-    return table   
+                        items.append(value)
+                rows.append(items)
+    print(pd.DataFrame(rows, columns=['Campo', 'Tipo', 'Longitud', 'Descripción']).to_dict('records'))
+    return pd.DataFrame(rows, columns=['Campo', 'Tipo', 'Longitud', 'Descripción'])
 
 dropdown_options = {
-    'tables': ['coordinates', 'yellow_taxi', 'green_taxi'],
+    'tables': ['active_vehicles_count', 'emissions', 'trips', 'weather', 'coordinates'],
 }
 
 layout = html.Div([
@@ -63,16 +55,35 @@ layout = html.Div([
                 dbc.FormText("Selecciona una tabla", className='text-muted p-0 m-0 ms-1'),
             ], className='mb-3'),                 
         ], width=3),
-        dbc.Col([html.Div(
-            json_table('coordinates'), id='table1')], width=9)                         
+        dbc.Col([
+            html.Div(
+                children=dash_table.DataTable(                        
+                        id='table1',
+                        columns =[
+                            {"name": i, "id": i} for i in ['Campo', 'Tipo', 'Longitud', 'Descripción']
+                        ],
+                        style_cell={'padding': '5px', 'fontSize': 12, 'textAlign': 'left'},
+                        style_data={
+                            'whiteSpace': 'normal',
+                            'height': 'auto',
+                            'lineHeight': '15px'
+                        },
+                        style_header={
+                            'backgroundColor': 'white',
+                            'fontWeight': 'bold'
+                        },
+                    ),
+            )
+            ], width=9)                         
     ], className='container-fluid')
 ], className='container-fluid ')
 
 @callback(
     [
-        Output('table1', 'table'),        
-        Input('table-dropdown', 'value')
+        Output('table1', 'data'),        
+        [Input('table-dropdown', 'value')]
     ] 
 )
 def update_table(selected_table):
-    return json_table(selected_table)
+    return json_table(selected_table).to_dict('records'),
+    
